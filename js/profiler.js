379196b163671fd27b659ef9d700fe2db389541a
gorhill <rhill@raymondhill.net>
2013-10-16 22:23:45 -0300

    added simple profiler tool

---
+++ js/profiler.js
@@ -0,0 +1,56 @@
+/*******************************************************************************
+
+    httpswitchboard - a Chromium browser extension to black/white list requests.
+    Copyright (C) 2013  Raymond Hill
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see {http://www.gnu.org/licenses/}.
+
+    Home: https://github.com/gorhill/httpswitchboard
+*/
+
+/******************************************************************************/
+
+function Profiler() {
+    this.time = 0;
+    this.count = 0;
+    this._start = 0;
+    this._lastlog = 0;
+}
+
+Profiler.prototype.reset = function() {
+    this.time = 0;
+    this.count = 0;
+    this._start = 0;
+};
+
+Profiler.prototype.start = function() {
+    this._start = Date.now();
+};
+
+Profiler.prototype.stop = function(s) {
+    var now = Date.now();
+    this.time += now - this._start;
+    this.count += 1;
+    if ( (now - this._lastlog) > 10000 ) {
+        console.log('HTTP Switchboard Profiler() > %s: %f ms per iteration', s, this.avg());
+        this._lastlog = now;
+    }
+};
+
+Profiler.prototype.avg = function() {
+    return this.count > 0 ? this.time / this.count : 0;
+};
+
+var qadProfiler = new Profiler();
+
