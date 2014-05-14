af91ee7af0b8c30405c01cb17ba2a471317ff073
gorhill <rhill@raymondhill.net>
2014-05-14 14:46:35 -0400

    fixed dashboard tab management

--- js/async.js
+++ js/async.js
@@ -138,29 +138,60 @@ function permissionsChanged() {
 /******************************************************************************/
 
 function gotoExtensionURL(url) {
-    url = chrome.extension.getURL(url);
+
+    var hasFragment = function(url) {
+        return url.indexOf('#') >= 0;
+    };
+
+    var removeFragment = function(url) {
+        var pos = url.indexOf('#');
+        if ( pos < 0 ) {
+            return url;
+        }
+        return url.slice(0, pos);
+    };
+
+    var tabIndex = 9999;
+    var targetUrl = chrome.extension.getURL(url);
+    var urlToFind = removeFragment(targetUrl);
+
+    var currentWindow = function(tabs) {
+        var updateProperties = { active: true };
+        var i = tabs.length;
+        while ( i-- ) {
+            if ( removeFragment(tabs[i].url) !== urlToFind ) {
+                continue;
+            }
+            // If current tab in dashboard is different, force the new one, if
+            // there is one, to be activated.
+            if ( tabs[i].url !== targetUrl ) {
+                if ( hasFragment(targetUrl) ) {
+                    updateProperties.url = targetUrl;
+                }
+            }
+            // Activate found matching tab
+            // Commented out as per:
+            // https://github.com/gorhill/httpswitchboard/issues/150#issuecomment-32683726
+            // chrome.tabs.move(tabs[0].id, { index: index + 1 });
+            chrome.tabs.update(tabs[i].id, updateProperties);
+            return;
+        }
+        chrome.tabs.create({ 'url': targetUrl, index: tabIndex + 1 });
+    };
+
+    var currentTab = function(tabs) {
+        if ( tabs.length ) {
+            tabIndex = tabs[0].index;
+        }
+        chrome.tabs.query({ currentWindow: true }, currentWindow);
+    };
+
     // https://github.com/gorhill/httpswitchboard/issues/150
     // Logic:
     // - If URL is already opened in a tab, just activate tab
     // - Otherwise find the current active tab and open in a tab immediately
     //   to the right of the active tab
-    chrome.tabs.query({ active: true }, function(tabs) {
-        var index = tabs.length ? tabs[0].index : 9999;
-        chrome.tabs.query({ currentWindow: true }, function(tabs) {
-            var i = tabs.length;
-            while ( i-- ) {
-                if ( tabs[i].url === url ) {
-                    // Activate found matching tab
-                    // Commented out as per:
-                    // https://github.com/gorhill/httpswitchboard/issues/150#issuecomment-32683726
-                    // chrome.tabs.move(tabs[0].id, { index: index + 1 });
-                    chrome.tabs.update(tabs[i].id, { active: true });
-                    return;
-                }
-            }
-            chrome.tabs.create({ 'url': url, index: index + 1 });
-        });
-    });
+    chrome.tabs.query({ active: true }, currentTab);
 }
 
 /******************************************************************************/
