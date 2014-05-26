e66f918f09d349fdc5664d14254796ca7ce65aaf
gorhill <rhill@raymondhill.net>
2014-05-25 21:13:01 -0400

    code review toward 0.9.5.3

--- js/background.js
+++ js/background.js
@@ -51,7 +51,7 @@ var HTTPSB = {
         spoofUserAgentWith: 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.154 Safari/537.36\nMozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.152 Safari/537.36\nMozilla/5.0 (Windows NT 6.1; WOW64; rv:28.0) Gecko/20100101 Firefox/28.0\nMozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.116 Safari/537.36\nMozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.116 Safari/537.36\n',
         statsFilters: {},
         strictBlocking: true,
-        subframeColor: '#c00',
+        subframeColor: '#cc0000',
         subframeOpacity: 100
     },
 
